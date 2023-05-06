a55ea3eb3d05ff8c83be10d3ec2d59dd7196f5bd
AndreyMatviets <asmatviets@gmail.com>
2023-05-06 15:35:02 +0400

    Initial Commit

---
+++ OpenBrewery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenBrewery
+//
+//  Created by Andrey Matviets on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
